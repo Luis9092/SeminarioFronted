96b23427ee59dd8dd55aee25954752d03762dbb5
Luis9092 <luisc522txe@gmail.com>
2024-10-11 01:51:06 -0600

    ProductoTerminado

--- main.py
+++ main.py
@@ -88,54 +88,122 @@ def iniciarSesion():
     # return redirect(url_for("root"))
 
 
-@app.route("/agregarProducto", methods=["POST"])
-def AgregarProducto():
+@app.route("/productosOperaciones/<string:accion>", methods=["POST"])
+def productosOperaciones(accion):
+    operacion = accion
     nombreproducto = request.form["nombreproducto"]
     file = request.files["imagenProducto"]
     txtprecio = request.form["txtprecio"]
     txtproveedor = request.form["txtProveedor"]
     txtcategoria = request.form["txtcategoria"]
     txtdescripcion = request.form["txtdescripcion"]
+    txtid = request.form["txtid"]
+    txtnameAnterior = request.form["txtnameAnterior"]
+
     nombre = file.filename
     pro = Producto()
     respuesta = {"mensaje": "Por favor revisar los datos.", "estado": 0}
-    if file:
-        filename = secure_filename(file.filename)
-        file.save("static/" + filename)
-        url = "http://127.0.0.1:8000/agregarProducto"
+    urlActualizar = "http://127.0.0.1:8000/modificarProducto"
+    if operacion == "agregar":
+        if file:
+            filename = secure_filename(file.filename)
+            file.save("static/" + filename)
+            url = "http://127.0.0.1:8000/agregarProducto"
 
-        # Parámetros a enviar
+            # Parámetros a enviar
 
-        # Hacer la solicitud POST con el archivo
-        with open("static/" + filename, "rb") as f:
-            # Archivo a subir
-            files = {"file": f}
+            # Hacer la solicitud POST con el archivo
+            with open("static/" + filename, "rb") as f:
+                # Archivo a subir
+                files = {"file": f}
 
-            # Hacer la solicitud POST
-            response = requests.post(
-                url,
-                files=files,
-                data={
-                    "nombre": nombreproducto,
-                    "descripcion": txtdescripcion,
-                    "precio": float(txtprecio),
-                    "proveedorid": int(txtproveedor),
-                    "idcategoria": int(txtcategoria),
-                },
-            )
-            if response.status_code == 200:
-                print("hecho con exito xdxd")
-                respuesta = {
-                    "mensaje": "Producto agregado correctamente!!",
-                    "estado": 1,
-                }
+                # Hacer la solicitud POST
+                response = requests.post(
+                    url,
+                    files=files,
+                    data={
+                        "nombre": nombreproducto,
+                        "descripcion": txtdescripcion,
+                        "precio": float(txtprecio),
+                        "proveedorid": int(txtproveedor),
+                        "idcategoria": int(txtcategoria),
+                    },
+                )
+                if response.status_code == 200:
+                    print("hecho con exito xdxd")
+                    respuesta = {
+                        "mensaje": "Producto agregado correctamente!!",
+                        "estado": 1,
+                    }
 
-    ruta = "static/" + nombre
-    if os.path.isfile(ruta):
-        os.remove(ruta)
-    pro.eliminarImagenResultadoServer(nombre)
+        ruta = "static/" + nombre
+        if os.path.isfile(ruta):
+            os.remove(ruta)
+        pro.eliminarImagenResultadoServer(nombre)
+
+    if operacion == "actualizar":
+        try:
+            archivo_temporal = ""
+            nombreTemporal = ""
+            if file.filename == "":
+                nombreTemporal = "empty_file.txt"
+                archivo_temporal = "static/" + "empty_file.txt"
+                filename = secure_filename("empty_file.txt")
+                with open("static/empty_file.txt", "w") as f:
+                    pass
+
+            else:
+                nombreTemporal = file.filename
+                archivo_temporal = "static/" + nombreTemporal
+                filename = secure_filename(nombreTemporal)
+                file.save("static/" + nombreTemporal)
+
+            with open(archivo_temporal, "rb") as f:
+
+                files = {"file": f}
+                response = requests.put(
+                    urlActualizar,
+                    files=files,
+                    data={
+                        "nombre": nombreproducto,
+                        "descripcion": txtdescripcion,
+                        "precio": float(txtprecio),
+                        "proveedorid": int(txtproveedor),
+                        "idcategoria": int(txtcategoria),
+                        "idproducto": int(txtid),
+                        "NombreImagen": str(txtnameAnterior),
+                    },
+                )
+
+                if response.status_code == 200:
+                    print("hecho con exito xdxd")
+                    respuesta = {
+                        "mensaje": "Producto modificado correctamente!!",
+                        "estado": 1,
+                    }
+
+        except Exception as e:
+            print(f"Ocurrió un error: {e}")
+            respuesta = {
+                "mensaje": "Error al modificar el producto.",
+                "estado": 0,
+            }
+    if operacion == "eliminar":
+        retorno = pro.eliminarProducto(id=txtid, nombreImagen=txtnameAnterior)
+        print(retorno)
+        if retorno == 1:
+            respuesta = {
+                "mensaje": "Producto Eliminado Correctamente",
+                "estado": 1,
+            }
+        else:
+            respuesta = {
+                "mensaje": "Por favor verificar el manual de incidencias",
+                "estado": 0,
+            }
 
     return json.dumps(respuesta)
+
 
 # Ver Proveedores
 @app.route("/proveedores")
@@ -149,11 +217,12 @@ def proveedores():
 
     if response.status_code == 200:
         tableProveedores = response.json()
-        return render_template("proveedores.html", tableProveedores=tableProveedores, per=per)
-    
+        return render_template(
+            "proveedores.html", tableProveedores=tableProveedores, per=per
+        )
+
     return render_template("proveedores.html", tableProveedores=[], per=per)
 
- 
 
 # Eliminar Proveedor
 @app.route("/eliminarProveedor/<int:proveedor_id>", methods=["POST"])
@@ -163,9 +232,15 @@ def eliminarProveedor(proveedor_id):
     response = requests.delete(url)
 
     if response.status_code == 200:
-        return json.dumps({"mensaje": f"Proveedor {proveedor_id} eliminado correctamente", "estado": 1})
+        return json.dumps(
+            {
+                "mensaje": f"Proveedor {proveedor_id} eliminado correctamente",
+                "estado": 1,
+            }
+        )
     else:
         return json.dumps({"mensaje": "Error al eliminar el proveedor.", "estado": 0})
+
 
 @app.route("/ventas")
 def ventas():
@@ -179,8 +254,9 @@ def ventas():
     if response.status_code == 200:
         tableVentas = response.json()
         return render_template("ventas.html", tableVentas=tableVentas, per=per)
-    
+
     return render_template("ventas.html", tableVentas=[], per=per)
+
 
 @app.route("/agregarVenta", methods=["POST"])
 def agregarVenta():
@@ -194,6 +270,7 @@ def agregarVenta():
     else:
         return json.dumps({"mensaje": "Error al agregar la venta.", "estado": 0})
 
+
 @app.route("/eliminarVenta/<int:venta_id>", methods=["POST"])
 def eliminarVenta(venta_id):
     url = f"http://127.0.0.1:8000/eliminarVenta/{venta_id}"
@@ -201,12 +278,14 @@ def eliminarVenta(venta_id):
     response = requests.delete(url)
 
     if response.status_code == 200:
-        return json.dumps({"mensaje": f"Venta {venta_id} eliminada correctamente", "estado": 1})
+        return json.dumps(
+            {"mensaje": f"Venta {venta_id} eliminada correctamente", "estado": 1}
+        )
     else:
         return json.dumps({"mensaje": "Error al eliminar la venta.", "estado": 0})
 
+
 # Ver Proveedores
- 
 
 
 # Agregar Proveedor
@@ -227,9 +306,6 @@ def agregarProveedor():
         respuesta = {"mensaje": "Error al agregar proveedor.", "estado": 0}
 
     return json.dumps(respuesta)
-
-
- 
 
 
 @app.route("/ventas")
--- model/productos.py
+++ model/productos.py
@@ -19,3 +19,23 @@ class Producto():
         except Exception as e:
             print(f"Ocurrió un error: {e}")
             return 0
+        
+    def extraer_nombre_imagen(self, url):
+        # Dividir la URL por '/' y obtener el último elemento
+        nombre_imagen = url.split('/')[-1]
+        return nombre_imagen
+    
+    def eliminarProducto(self,  id, nombreImagen):
+        nombre = self.extraer_nombre_imagen(nombreImagen)
+        
+        api_url = "http://127.0.0.1:8000/eliminarproducto"
+        url = f"{api_url}/{id}/{nombre}"
+        
+        try:
+            response = requests.delete(url)
+            if response.status_code == 200:
+                return 1
+            return 0   
+        except Exception as e:
+            print(f"Ocurrió un error: {e}")
+            return 0
